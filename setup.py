70a9b2cc20c942e1bd2edad619bd44e76b1681aa
Stefanos Kyrikakis <kirikakis@gmail.com>
2025-03-07 17:14:04 +0000

    add a cmakelists file to generate the service files

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'hailo_rpi_ros2 = hailo_rpi_ros2_pkg.ros2_node:main',
+            'ros2_node = hailo_rpi_ros2.ros2_node:main',
         ],
     },
 )
